c56516457c9a3e6e140af015da424a87f8538db4
sambhartmann <94196351+sambhartmann@users.noreply.github.com>
2021-11-19 15:27:46 -0500

    Update State.java

--- Project5/src/prj5/State.java
+++ Project5/src/prj5/State.java
@@ -2,9 +2,6 @@
 package prj5;
 
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 /**
  * @author Sam Hartmann, Annalise Gellene, Josh Sapirstein
