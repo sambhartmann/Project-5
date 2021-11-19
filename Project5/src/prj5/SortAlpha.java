ef93f61bfe6fd8f59b8adbcfaef3d6fab868401d
sambhartmann <94196351+sambhartmann@users.noreply.github.com>
2021-11-19 15:23:49 -0500

    Update and rename CompareAlpha.java to SortAlpha.java

---
+++ Project5/src/prj5/SortAlpha.java
@@ -0,0 +1,20 @@
+package prj5;
+
+import java.util.Comparator;
+
+public class SortAlpha implements Comparator<Race> {
+
+    /**
+     * compares two race objects by their name
+     * 
+     * @param a
+     *            the first race
+     * @param b
+     *            the race that the first race is being compared two
+     */
+    public int compare(Race a, Race b) {
+        String race1 = a.getName();
+        String race2 = b.getName();
+        return race1.compareTo(race2);
+    }
+}
